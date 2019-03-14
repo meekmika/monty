8aa36dedb0c0e7f75994882ffd79e3cf451d9aa5
Marika Driman <marikadriman@gmail.com>
2019-03-14 13:05:16 +0100

    alternative solution

--- monty-kotlin/Monty.kt
+++ monty-kotlin/Monty.kt
@@ -1,4 +1,6 @@
-import java.util.Random
+import java.util.*
+
+val rand = Random()
 
 fun main(args: Array<String>) {
     val totalGames = args[0].toInt()
@@ -9,6 +11,10 @@ fun main(args: Array<String>) {
 
     println("Wins by staying: ${wins.first} - $stayPercentage%")
     println("Wins by switching: ${wins.second} - $switchPercentage%")
+
+    val boxCount = 100
+    println("\nAlt. running $totalGames simulations with $boxCount doors")
+    montyhall(boxCount = boxCount, totalGames = totalGames)
 }
 
 fun monty(totalGames: Int): Pair<Int, Int> {
@@ -38,4 +44,28 @@ fun remainingBox(boxA: Int, boxB: Int): Int {
     } while (remainingBox == boxA || remainingBox == boxB)
 
     return remainingBox
+}
+
+fun montyhall(totalGames: Int = 10000, boxCount: Int = 3) {
+    var switchWins = 0
+    var stayWins = 0
+
+    (1..totalGames).forEach {
+        val boxes = MutableList(boxCount) { 0 }
+        boxes[rand.nextInt(boxCount)] = 1
+
+        val pick = rand.nextInt(boxCount)
+
+        boxes.removeAt(pick)
+
+        val switch = boxes.contains(1)
+
+        if (switch) switchWins++ else stayWins++
+    }
+
+    val stayPercentage = "%.2f".format(stayWins / totalGames.toDouble() * 100)
+    val switchPercentage = "%.2f".format(switchWins / totalGames.toDouble() * 100)
+
+    println("Wins by staying: $stayWins - $stayPercentage%")
+    println("Wins by switching: $switchWins - $switchPercentage%")
 }
